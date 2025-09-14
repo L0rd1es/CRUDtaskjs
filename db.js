73337b828cacbdd8b10b0c3e4fc49c8d5cdf1c7c
L0rd1es <81041515+L0rd1es@users.noreply.github.com>
2025-09-14 14:49:16 +0200

    Fixes

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
-//db.js
+require("dotenv").config();
 
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
-  password: "8213",
+  password: process.env.PGPASSWORD,
   host: "localhost",
   port: 5432,
   database: "node_postgres",
